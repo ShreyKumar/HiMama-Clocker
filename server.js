ad55bfed29d3f32b68d1bde70456a44419964ea3
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 18:47:22 -0500

    doing it the old fashioned way

--- server.js
+++ server.js
@@ -33,9 +33,11 @@ app.get("/clock/get", (req, res) => {
 })
 
 // Only for deployment
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
