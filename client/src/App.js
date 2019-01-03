edbd94c9fc867545807061b085ada8de5e15cb34
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 00:05:32 -0500

    finished clocks

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo_header.png';
+import DigitalClock from "react-digital-clock";
+import AnalogClock from "react-clock";
+import "./App.scss"
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    //change analog clock
+    setInterval(
+      () => this.setState({date: new Date()}),
+      1000
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="main-container">
+          <header>
+            <img src={logo} />
+            <h1>
+              <span className="first">Clock</span>
+              <span className="second">r</span>
+            </h1>
+          </header>
+          <div className="clocks">
+            <div className="digital-clock">
+              <DigitalClock />
+            </div>
+            <AnalogClock value={this.state.date} size={400} className="analog-clock" />
+          </div>
+          
+
+        </div>
+
       </div>
     );
   }
