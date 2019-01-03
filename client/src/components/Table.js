dd1cc956745519aaef5523f03c61e2ea8b3e4fd7
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 02:01:00 -0500

    added some restructuring + finished form button

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -4,6 +4,19 @@ import "./../scss/Table.scss";
 class Table extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      "inoutswitch": "in"
+    }
+
+    this.inOutSwitch = this.inOutSwitch.bind(this)
+  }
+
+  inOutSwitch(){
+    if(this.state.inoutswitch == "in"){
+      this.setState({"inoutswitch": "out"})
+    } else {
+      this.setState({"inoutswitch": "in"})
+    }
   }
 
   render(){
@@ -11,16 +24,16 @@ class Table extends Component {
       <form className="clock-form">
         <input className="firstname" placeholder="Firstname" />
         <input className="lastname" placeholder="Lastname"/>
-        <div className="inorout">
+        <div className="inorout" onClick={this.inOutSwitch}>
           <div className="in">
-            <a href="#">IN</a>
+            <span>IN</span>
           </div>
           <div className="out">
-            <a href="#">OUT</a>
+            <span>OUT</span>
           </div>
-          <div className="selected"></div>
+          <div className={"selected " + this.state.inoutswitch}></div>
         </div>
-        <button className="submit-btn">Submit</button>
+        <a href="#" className="submit-btn">Submit</a>
       </form>
     )
   }
