1a257f4881b5d6223d5848d6f4dc571bf515f1fd
Shreyansh <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 20:07:19 -0500

    trying to remove production

--- server.js
+++ server.js
@@ -35,11 +35,9 @@ app.get("/clock/get", (req, res) => {
   res.send(users.allusers)
 })
 // Only for production
-if (process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"));
   app.get('*', function(req, res) {
     res.sendFile(path.join('client/build', 'index.html'));
   });
-}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
