e925fa771e922ced9a404bc426fe5e9e687c61ae
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2019-01-04 01:27:51 -0500

    created edit functionality almost finished

--- client/src/components/ui/Card.js
+++ client/src/components/ui/Card.js
@@ -6,56 +6,126 @@ class Card extends Component {
   constructor(props){
     super(props)
 
+    let d = new Date(this.props.data.time)
+    let hours = d.getHours()
+    let ampm = ""
+    if(hours > 12){
+      hours -= 12
+      ampm = "PM"
+    } else if(hours == 0){
+      hours = 12
+      ampm = "AM"
+    } else {
+      ampm = "AM"
+    }
+    let mins = d.getMinutes()
+    let secs = d.getSeconds()
+
     this.state = {
       "editmode": false,
-      data: this.props.data
+      "firstname": this.props.data.firstname,
+      "lastname": this.props.data.lastname,
+      "mode": this.props.data.mode,
+      "hours": hours,
+      "mins": mins,
+      "secs": secs,
+      "ampm": ampm,
+      "fieldsvalid": false
     }
 
     this.editMode = this.editMode.bind(this)
+    this.updateFirstname = this.updateFirstname.bind(this)
+    this.updateLastname = this.updateLastname.bind(this)
+    this.updateMode = this.updateMode.bind(this)
+    this.updateHours = this.updateHours.bind(this)
+    this.updateMinutes = this.updateMinutes.bind(this)
+    this.updateSeconds = this.updateSeconds.bind(this)
+    this.updateAMPM = this.updateAMPM.bind(this)
+
+    this.save = this.save.bind(this)
+
+    this.namechecker = /[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-{1,}]/;
   }
 
   editMode(){
     this.setState({"editmode": true})
   }
 
+  updateFirstname(e){
+    this.setState({
+      firstname: e.target.value
+    })
+  }
+
+  updateLastname(e){
+    this.setState({
+      lastname: e.target.value
+    })
+  }
+
+  updateMode(mode){
+    this.setState({
+      mode: mode
+    })
+  }
+
+  updateHours(e){
+    this.setState({
+      hours: e.target.value
+    })
+  }
+
+  updateMinutes(e){
+    this.setState({
+      mins: e.target.value
+    })
+  }
+
+  updateSeconds(e){
+    this.setState({
+      secs: e.target.value
+    })
+  }
+
+  updateAMPM(e){
+    this.setState({
+      ampm: e.target.value
+    })
+  }
+
+  save(e){
+    if(e.keyCode == 13){
+      alert("enter")
+    }
+  }
+
+
 
   render(){
     if(this.state.editmode){
-      let d = new Date(this.props.data.time)
-      let hours = d.getHours()
-      let ampm = ""
-      if(hours > 12){
-        hours -= 12
-        ampm = "PM"
-      } else if(hours == 0){
-        hours = 12
-        ampm = "AM"
-      } else {
-        ampm = "AM"
-      }
-      let mins = d.getMinutes()
-      let secs = d.getSeconds()
+      console.log("edit mode on")
 
       return (
-        <div className="Card editmode">
-          <input className="firstname" type="text" value={this.state.data.firstname} />
-          <input className="lastname" type="text" value={this.state.data.lastname} />
-          <Switch toggle={this.state.data.mode} />
-          <input type="text" className="hours" value={hours} />
+        <div className="Card editmode" onKeyUp={this.save}>
+          <input onChange={this.updateFirstname} className="firstname" type="text" value={this.state.firstname} />
+          <input onChange={this.updateLastname} className="lastname" type="text" value={this.state.lastname} />
+          <Switch sendSwitchValue={this.updateMode} toggle={this.state.mode} />
+          <input onChange={this.updateHours} type="text" className="hours" value={this.state.hours} />
           <span className="mark">:</span>
-          <input type="text" className="mins" value={mins} />
+          <input onChange={this.updateMinutes} type="text" className="mins" value={this.state.mins} />
           <span className="mark">:</span>
-          <input type="text" className="secs" value={secs} />
-          <input type="text" className="ampm" value={ampm} />
+          <input onChange={this.updateSeconds} type="text" className="secs" value={this.state.secs} />
+          <input onChange={this.updateAMPM} type="text" className="ampm" value={this.state.ampm} />
         </div>
       )
     } else {
+      console.log("edit mode off")
       return(
         <div className="Card">
-          <p onDoubleClick={this.editMode} className="item firstname">{this.state.data.firstname}</p>
-          <p onDoubleClick={this.editMode} className="item lastname">{this.state.data.lastname}</p>
-          <p onDoubleClick={this.editMode} className={"item mode " + this.state.data.mode}>{this.state.data.mode.toUpperCase()}</p>
-          <p onDoubleClick={this.editMode} className="item time">{new Date(this.state.data.time).toLocaleTimeString()}</p>
+          <p onDoubleClick={this.editMode} className="item firstname">{this.props.data.firstname}</p>
+          <p onDoubleClick={this.editMode} className="item lastname">{this.props.data.lastname}</p>
+          <p onDoubleClick={this.editMode} className={"item mode " + this.props.data.mode}>{this.props.data.mode.toUpperCase()}</p>
+          <p onDoubleClick={this.editMode} className="item time">{new Date(this.props.data.time).toLocaleTimeString()}</p>
         </div>
       )
     }
