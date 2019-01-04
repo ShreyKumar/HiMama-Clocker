07332f6b51065eb5d68604adbff9b486cb63a84e
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 20:00:55 -0500

    another try

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.get("/clock/get", (req, res) => {
 })
 // Only for production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join('client/build', 'index.html'));
   });
 }
 
