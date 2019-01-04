656b3785edfd75e9b6cdbddf0a054e6d093db0df
Shreyansh <shreyansh.kumar@mail.utoronto.ca>
2019-01-03 20:08:51 -0500

    back to normal

--- server.js
+++ server.js
@@ -34,10 +34,15 @@ app.post("/clock/out", (req, res) => {
 app.get("/clock/get", (req, res) => {
   res.send(users.allusers)
 })
+
 // Only for production
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join('client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
